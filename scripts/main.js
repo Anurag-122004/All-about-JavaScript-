1aa05b57040c2becf1c66b5dc5d7bc39ffb78597
Anurag Kaushik <anuragofficial.wrk@gmail.com>
2024-02-24 00:25:59 +0530

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,32 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
-let x = 42;
+let x = 5;
 console.log(x);
 if (true) {
     var y = 5;
 }
 console.log(y);
+let mycar = "bob";
+mycar = "tom";
+console.log(mycar);
+let vari = true;
+let vari1 = 5;
+let vari2 = "hello";
+let dsa = [1,"bob",2,"secc"];
+console.log(vari);
+console.log(vari1);
+console.log(vari2);
+console.log(dsa);
+
+if ( vari1 === x){
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+let ice = "chosc";
+if (ice == "choc"){
+    alert("yes i love choc");
+} else {
+    alert("baw i love choc");
+}
