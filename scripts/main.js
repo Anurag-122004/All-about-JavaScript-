453f426bf4e01a0d94dd41c6b56aab4177952bcf
Anurag Kaushik <anuragofficial.wrk@gmail.com>
2024-02-24 16:36:21 +0530

    condtional, Loops, string completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,77 +1,174 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-let x = 5;
-console.log(x);
-if (true) {
-    var y = 5;
-}
-console.log(y);
-let mycar = "bob";
-mycar = "tom";
-console.log(mycar);
-let vari = true;
-let vari1 = 5;
-let vari2 = "hello";
-let dsa = [1,"bob",2,"secc"];
-console.log(vari);
-console.log(vari1);
-console.log(vari2);
-console.log(dsa);
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+// let x = 5;
+// console.log(x);
+// if (true) {
+//     var y = 5;
+// }
+// console.log(y);
+// let mycar = "bob";
+// mycar = "tom";
+// console.log(mycar);
+// let vari = true;
+// let vari1 = 5;
+// let vari2 = "hello";
+// let dsa = [1,"bob",2,"secc"];
+// console.log(vari);
+// console.log(vari1);
+// console.log(vari2);
+// console.log(dsa);
 
-if ( vari1 === x){
-    console.log("true");
-} else {
-    console.log("false");
-}
+// if ( vari1 === x){
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
 
-let ice = "chosc";
-if (ice == "choc"){
-    alert("yes i love choc");
-} else {
-    alert("baw i love choc");
-}
+// let ice = "chosc";
+// if (ice == "choc"){
+//     alert("yes i love choc");
+// } else {
+//     alert("baw i love choc");
+// }
 
-function multiply (num1,num2){
-    let result = (num1 * num2);
-    return result;
-}
+// function multiply (num1,num2){
+//     let result = (num1 * num2);
+//     return result;
+// }
 
-document.querySelector("html").addEventListener("click", () => {
-    alert("arrow function created")
-})
+// document.querySelector("html").addEventListener("click", () => {
+//     alert("arrow function created")
+// })
 
-let myButton = document.querySelector("button");
+// let myButton = document.querySelector("button");
 
-// myButton.addEventListener("click" , () => {
-//     prompt("enter your name");
-// });
+// // myButton.addEventListener("click" , () => {
+// //     prompt("enter your name");
+// // });
 
-myButton.onclick = () => {
-    prompt("enter name");
-};
+// myButton.onclick = () => {
+//     prompt("enter name");
+// };
 
-let e = BigInt("5");
-let f = Symbol("tommm");
+// let e = BigInt("5");
+// let f = Symbol("tommm");
 
-const student = {
-    name : "annu",
-    age : 5,
-    marks : 99,
-};
+// const student = {
+//     name : "annu",
+//     age : 5,
+//     marks : 99,
+// };
 
-const product = {
-    productName : "Parker Jotter Standard",
-    rating : 7002,
-    productPrice : 270,
-    discount : 1.35,
-};
+// const product = {
+//     productName : "Parker Jotter Standard",
+//     rating : 7002,
+//     productPrice : 270,
+//     discount : 1.35,
+// };
 
-const profile = {
-    name : "Anurag Kaushik",
-    options : "Follow or Message",
-    posts : 195,
-    followers : "569k",
-    following : 4,
-    profession : "Entrepreneur",
-    description : "DRDO, Ex-Microsoft, TedX Speaker",
-};
+// const profile = {
+//     name : "Anurag Kaushik",
+//     options : "Follow or Message",
+//     posts : 195,
+//     followers : "569k",
+//     following : 4,
+//     profession : "Entrepreneur",
+//     description : "DRDO, Ex-Microsoft, TedX Speaker",
+// };
+
+// let ww = 5;
+// let ew = 5;
+// console.log("a+b=", ww + ew);
+
+// let agess = 25;
+// if (agess > 18) {
+//     console.log("yes you can vote");
+// } else {
+//     console.log("you can't vote ");
+// }
+// let uncle = 8;
+
+// console.log(uncle > 55 ? "budha" : "javan");
+
+// const fruit = "oranges";
+// switch(fruit) {
+//     case "mango":
+//         console.log("price is 55");
+//         break;
+//     case "grapes":
+//     case "oranges":
+//         console.log("price of both is 45");
+//         break;
+//     default: console.log("no price due to error");
+// }
+
+// let number = prompt("enter a number");
+// if ( number % 5 == 0 ){
+//     console.log("yes it is multiple") ;
+// } else {
+//     console.log("not a multilpe");
+// }
+
+// let grades = prompt("enter your grades?");
+// if ( grades > 95){
+//     console.log("A+");
+// } else if (grades < 95 && grades > 90){
+//     console.log("A");
+// } else if (grades < 90 && grades > 80){
+//     console.log("B");
+// } else if (grades < 80 && grades > 70){
+//     console.log("C");
+// } else {
+//     console.log("D");
+// }
+// let sum = 0;
+// let n = prompt("enter your number");
+// for (let i=1; i<=n; i++){
+//     sum+=i;
+//     // console.log("Anurag Kaushik");
+// }
+// console.log("sum = ",sum);
+// let n = prompt("enter your number");
+// while (n <= 5){
+//     console.log("Anurag Kaushik");
+//     n++;
+// }
+// let stringv = "Anurag Kaushik";
+// for (let i in stringv){
+//     console.log("Value = ", i);
+// }
+// for ( let i=0; i<=100; i++){
+//     if ( i % 2 === 0 ){
+//         console.log("Even number = ", i);
+//     } else {
+//         console.log("odd Number = ", i);
+//     }
+// }
+// let correctValue = 12;
+// let userValue = prompt("enter your value");
+// while ( userValue != correctValue ){
+//     userValue = prompt("you entered wrong value , guess again");
+// } 
+// console.log("Congrts, You won..", userValue);
+// let str = `AnuragKaushik`;
+// console.log(str);
+
+// const purchase = {
+//     item : "pen",
+//     price : 10
+// }
+// console.log(`The price of ${purchase.item} is ${purchase.price} Rs.`);
+// console.log("The Price of ", purchase.item ," is ", purchase.price, " Rs.");
+// let str = " Anurag Kaushik ";
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.trim());
+// console.log(str.slice(2,5));
+// console.log(str.replace("nur","run"));
+// console.log(str.charAt(5));
+let username = prompt("Enter your full name");
+console.log(username);
+let len = username.length;
+let result = "@"+username + len;
+console.log(result);
