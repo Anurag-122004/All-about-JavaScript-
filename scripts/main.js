a4dae2d514f4a496847f282e14ec789a51eb8615
Anurag Kaushik <anuragofficial.wrk@gmail.com>
2024-02-24 11:00:58 +0530

    click even

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,51 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-let x = 5;
-console.log(x);
-if (true) {
-    var y = 5;
-}
-console.log(y);
-let mycar = "bob";
-mycar = "tom";
-console.log(mycar);
-let vari = true;
-let vari1 = 5;
-let vari2 = "hello";
-let dsa = [1,"bob",2,"secc"];
-console.log(vari);
-console.log(vari1);
-console.log(vari2);
-console.log(dsa);
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+// let x = 5;
+// console.log(x);
+// if (true) {
+//     var y = 5;
+// }
+// console.log(y);
+// let mycar = "bob";
+// mycar = "tom";
+// console.log(mycar);
+// let vari = true;
+// let vari1 = 5;
+// let vari2 = "hello";
+// let dsa = [1,"bob",2,"secc"];
+// console.log(vari);
+// console.log(vari1);
+// console.log(vari2);
+// console.log(dsa);
 
-if ( vari1 === x){
-    console.log("true");
-} else {
-    console.log("false");
-}
+// if ( vari1 === x){
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
 
-let ice = "chosc";
-if (ice == "choc"){
-    alert("yes i love choc");
-} else {
-    alert("baw i love choc");
-}
+// let ice = "chosc";
+// if (ice == "choc"){
+//     alert("yes i love choc");
+// } else {
+//     alert("baw i love choc");
+// }
+
+// function multiply (num1,num2){
+//     let result = (num1 * num2);
+//     return result;
+// }
+
+// document.querySelector("html").addEventListener("click", () => {
+//     alert("arrow function created")
+// })
+
+let myButton = document.querySelector("button");
+
+// myButton.addEventListener("click" , () => {
+//     prompt("enter your name");
+// });
+
+myButton.onclick = () => {
+    prompt("enter name");
+};
